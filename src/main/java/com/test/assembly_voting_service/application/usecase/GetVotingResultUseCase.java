c40bf9a254760f866bcf892215a6718a4f3076e8
rnakamura <rkn.nakamura@gmail.com>
2026-08-20 09:33:02 -0300

    feat: adicionar parcialmente caso de uso para contabilizar resultado

---
+++ src/main/java/com/test/assembly_voting_service/application/usecase/GetVotingResultUseCase.java
@@ -0,0 +1,11 @@
+package com.test.assembly_voting_service.application.usecase;
+
+import com.test.assembly_voting_service.application.usecase.command.GetVotingResultQuery;
+
+public class GetVotingResultUseCase {
+
+    public Object execute(GetVotingResultQuery query) {
+        // TODO: Retornaremos os dados consolidados (contagem de votos e resultado APPROVED/REJECTED)
+        return new Object();
+    }
+}
