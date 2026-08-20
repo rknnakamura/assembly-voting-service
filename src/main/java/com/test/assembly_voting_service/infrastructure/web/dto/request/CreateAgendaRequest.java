85255679ea09ec505bb163afd0a2c5a8ad08e9e8
rnakamura <rkn.nakamura@gmail.com>
2026-08-20 17:21:31 -0300

    feat: adicionar schemas para melhorar documentacão informando exemplos

--- src/main/java/com/test/assembly_voting_service/infrastructure/web/dto/request/CreateAgendaRequest.java
+++ src/main/java/com/test/assembly_voting_service/infrastructure/web/dto/request/CreateAgendaRequest.java
@@ -1,8 +1,11 @@
 package com.test.assembly_voting_service.infrastructure.web.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
+@Schema(description = "Requisição para criação de uma nova pauta")
 public record CreateAgendaRequest(
+        @Schema(description = "Título descritivo da pauta", example = "Eleição para síndico 2026")
         @NotBlank(message = "Title is required") 
         String title
 ) {
