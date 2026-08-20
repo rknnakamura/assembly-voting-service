af1b41a14dcdfa6f76837438977fe00dbfcd8a1f
rnakamura <rkn.nakamura@gmail.com>
2026-08-20 09:16:08 -0300

    feat: adicionar caso de uso de listagem de agenda

--- src/main/java/com/test/assembly_voting_service/application/port/out/AgendaRepository.java
+++ src/main/java/com/test/assembly_voting_service/application/port/out/AgendaRepository.java
@@ -2,6 +2,9 @@ package com.test.assembly_voting_service.application.port.out;
 
 import com.test.assembly_voting_service.domain.model.agenda.Agenda;
 
+import java.util.List;
+
 public interface AgendaRepository {
     Agenda save(Agenda agenda);
+    List<Agenda> findAll();
 }
